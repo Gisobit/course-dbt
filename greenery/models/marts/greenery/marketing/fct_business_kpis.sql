1d46f67faa12d55e197f74321c088214345266d7
Gisobit <g.sotos@thebeat.co>
2022-06-17 09:45:50 +0000

    adding week2 answers

--- greenery/models/marts/greenery/marketing/fct_business_kpis.sql
+++ greenery/models/marts/greenery/marketing/fct_business_kpis.sql
@@ -11,4 +11,6 @@ select
 count(
   case when orders_done >= 2 then user_id end ) / (count(user_id))::real as repeat_rate
   , avg(avg_order_cost) avg_order_value_per_customer
+  , count(distinct user_id) total_customers
+  , sum(orders_done) total_orders_so_far
 from {{ ref('greenery', 'int_user_orders') }}
